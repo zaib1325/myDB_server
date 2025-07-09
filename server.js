387027166356b557a84a308271b387c5d25a44f7
zaib1325 <workplace3366@gmail.com>
2025-07-09 15:10:36 +0500

    hanzlaBhi's-Version

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
-
+const express = require('express');
+const jsonServer = require('json-server');
+const app = express();
+const port = process.env.PORT || 3000;
+// Middleware for JSON Server
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+app.use(middlewares);
+app.use('/api', router); // Access API at /api/Contacts, etc.
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
+    res.send('Welcome to My DB Server API. Visit /api/Contacts');
+});
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Server is running on port ${port}`);
+});
