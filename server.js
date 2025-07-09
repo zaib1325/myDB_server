d1f87e0df69b23ef58aae84409fdc0c5cc4e4b37
zaib1325 <workplace3366@gmail.com>
2025-07-09 14:12:15 +0500

    fix

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+// server.js
 const express = require('express');
 const jsonServer = require('json-server');
 const path = require('path');
 
 const app = express();
-const router = jsonServer.router(path.join(__dirname, 'db.json')); // your JSON file
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults();
 
 const PORT = process.env.PORT || 3001;
