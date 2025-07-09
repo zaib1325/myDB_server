f5c4b3f0a14740a3bf5d36e04e9a0457c4bc0de4
zaib1325 <workplace3366@gmail.com>
2025-07-09 13:02:40 +0500

    another-fix1

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
+const express = require('express');
 const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const path = require('path');
+
+const app = express();
+const router = jsonServer.router(path.join(__dirname, 'db.json')); // your JSON file
 const middlewares = jsonServer.defaults();
 
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(middlewares);
 app.use(router);
 
-app.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
